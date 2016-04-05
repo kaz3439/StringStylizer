d06ca3787e41f02e94265ad5c18f5d2734524ddc
Kazuhiro Hayashi <k.hayashi.info@gmail.com>
2016-04-06 00:18:12 +0900

    generate framework

---
+++ Carthage/Build/iOS/StringStylizer.framework/Headers/StringStylizer.h
@@ -0,0 +1,19 @@
+//
+//  StringStylizer.h
+//  StringStylizer
+//
+//  Created by Kazuhiro Hayashi on 4/4/16.
+//  Copyright © 2016 Kazuhiro Hayashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StringStylizer.
+FOUNDATION_EXPORT double StringStylizerVersionNumber;
+
+//! Project version string for StringStylizer.
+FOUNDATION_EXPORT const unsigned char StringStylizerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringStylizer/PublicHeader.h>
+
+
